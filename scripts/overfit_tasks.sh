01c7acc10506af34f77316bd4f3f2db29d71a80d
SymbiontConjurer <135414057+SymbiontConjurer@users.noreply.github.com>
2024-11-26 23:30:08 -0800

    Make it green so I can see it better in the mess of terminal messages

--- scripts/overfit_tasks.sh
+++ scripts/overfit_tasks.sh
@@ -36,6 +36,6 @@ fi
 for (( i=START_INDEX; i<=END_INDEX; i++ ))
 do
   TASK_ID=${TASKS[$i]}
-echo "Processing task ID: $TASK_ID ($((i - START_INDEX + 1))/$NUM_TASKS)"
+  echo -e "\e[32mProcessing task ID: $TASK_ID ($((i - START_INDEX + 1))/$NUM_TASKS)\e[0m"
   python src/train.py --config-path=configs/nick --config-name=overfit-ga training.task_generator.overfit_task="$TASK_ID"
 done
